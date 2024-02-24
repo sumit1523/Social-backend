cd17c302e517069b26f4328d08c93dc9633deabb
sumit kumar <sumit1523@gmail.com>
2024-02-24 21:33:17 +0530

    basic middlesware setup and DB connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // require("dotenv").config({path:'./env'}); \\require syntax
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`App is listening on port: ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO DB connection failed !! ", err);
+    });
 
 // Another way of connecting DB
 
